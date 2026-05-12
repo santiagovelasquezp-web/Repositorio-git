15b1079eed10b7403ff8bb671ef84240b93c3499
Simon <simon.velasquezp@uqvirtual.edu.co>
2026-05-12 12:24:46 -0500

    realizacion de clases

---
+++ src/model/Usuario.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Usuario {
+    private String ID;
+    private String nombre;
+    private String correo;
+    private String numeroTelefono;
+}
+//   un conjunto de métodos de pago simulados.
